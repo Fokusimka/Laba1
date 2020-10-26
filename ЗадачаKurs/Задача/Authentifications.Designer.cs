7379f9eb1795d8742f27497bca06bed69c2215ed
Fokus245 <59822576+Fokus245@users.noreply.github.com>
2020-10-26 23:18:38 +0600

    Add files via upload
    Парадигма с готовой UX-диаграммой и проект VS с началом разработки приложения

---
+++ ЗадачаKurs/Задача/Authentifications.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Задача
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Register = new System.Windows.Forms.Button();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.Logining = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Register
+            // 
+            this.Register.Location = new System.Drawing.Point(15, 68);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(132, 23);
+            this.Register.TabIndex = 0;
+            this.Register.Text = "Зарегестрироваться";
+            this.Register.UseVisualStyleBackColor = true;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(69, 12);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(159, 20);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(69, 42);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(159, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Location = new System.Drawing.Point(12, 15);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(44, 13);
+            this.Login.TabIndex = 3;
+            this.Login.Text = "Логин: ";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(12, 45);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(51, 13);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Пароль: ";
+            // 
+            // Logining
+            // 
+            this.Logining.Location = new System.Drawing.Point(153, 68);
+            this.Logining.Name = "Logining";
+            this.Logining.Size = new System.Drawing.Size(75, 23);
+            this.Logining.TabIndex = 5;
+            this.Logining.Text = "Войти";
+            this.Logining.UseVisualStyleBackColor = true;
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 104);
+            this.Controls.Add(this.Logining);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.Register);
+            this.Name = "Authorization";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button Logining;
+    }
+}
+
